fd06f644f30687e686ec07e2a99246d1e91b905b
Prince Adom <adomprince@gmail.com>
2019-02-16 22:30:57 -0500

    adding a second a screen to the main_activity.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button secScrn;
 
     public void switchCat(View view){
         ImageView catFemale = findViewById(R.id.catMale_ID);
+        TextView  displayText = findViewById(R.id.displayTxt);
+        String message = getString(R.string.female_cat_description);
+        displayText.setText(message);
         catFemale.setImageResource(R.drawable.cat_female);
 
     }
@@ -18,5 +25,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        secScrn = findViewById(R.id.nxtScrn_btn_ID);
+        secScrn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                secondScreen();
+            }
+        });
+    }
+
+    public void secondScreen(){
+        Intent intent = new Intent(this, DisplaySecondScreen.class);
+        startActivity(intent);
+
     }
 }
